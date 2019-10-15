44e6a6f7f1be602baef2fa3c7caa394599aebaa1
Lelala <lelala@ischool.com.tw>
2019-10-15 20:07:24 +0800

    修正可能的單引號問題

--- CalculateRegularAssessmentRank.cs
+++ CalculateRegularAssessmentRank.cs
@@ -552,13 +552,13 @@ WHERE
                 //每一筆學生先組好先加進List裡
                 studentSqlList.Add(@"
     SELECT
-        '" + row.Tag + @"'::BIGINT AS student_id
-        ,'" + row.Cells[3].Value + @"'::TEXT AS student_name
-        ,'" + ("" + row.Cells[4].Value).Trim('年', '級') + @"'::INT AS rank_grade_year
-        ,'" + "" + row.Cells[5].Value + @"'::TEXT AS rank_dept_name
-        ,'" + "" + row.Cells[6].Value + @"'::TEXT AS rank_class_name
-        ,'" + "" + row.Cells[7].Value + @"'::TEXT AS rank_tag1
-        ,'" + "" + row.Cells[8].Value + @"'::TEXT AS rank_tag2
+        '" + ("" + row.Tag).Replace("'", "''") + @"'::BIGINT AS student_id
+        ,'" + ("" + row.Cells[3].Value).Replace("'", "''") + @"'::TEXT AS student_name
+        ,'" + ("" + row.Cells[4].Value).Trim('年', '級').Replace("'", "''") + @"'::INT AS rank_grade_year
+        ,'" + ("" + row.Cells[5].Value).Replace("'", "''") + @"'::TEXT AS rank_dept_name
+        ,'" + ("" + row.Cells[6].Value).Replace("'", "''") + @"'::TEXT AS rank_class_name
+        ,'" + ("" + row.Cells[7].Value).Replace("'", "''") + @"'::TEXT AS rank_tag1
+        ,'" + ("" + row.Cells[8].Value).Replace("'", "''") + @"'::TEXT AS rank_tag2
     ");
             }
 
@@ -643,11 +643,11 @@ WHERE
                         //每一筆row(包含GradeYear, SchoolYear, Semester, ExamName)先組好加進List
                         rowSqlList.Add(@"
 	SELECT
-		'" + gradeYearList[index] + @"'::TEXT  AS rank_grade_year
-		, '" + schoolYear + @"'::TEXT AS rank_school_year
-		, '" + semester + @"'::TEXT AS rank_semester
-        , '" + examId + @"'::TEXT AS ref_exam_id
-		, '" + examName + @"'::TEXT AS rank_exam_name
+		'" + ("" + gradeYearList[index]).Replace("'", "''") + @"'::TEXT  AS rank_grade_year
+		, '" + ("" + schoolYear).Replace("'", "''") + @"'::TEXT AS rank_school_year
+		, '" + ("" + semester).Replace("'", "''") + @"'::TEXT AS rank_semester
+        , '" + ("" + examId).Replace("'", "''") + @"'::TEXT AS ref_exam_id
+		, '" + ("" + examName).Replace("'", "''") + @"'::TEXT AS rank_exam_name
         , '" + settingEle.OuterXml.Replace("'", "''") + @"'::TEXT AS calculation_setting
 ");
                     }
@@ -1872,16 +1872,20 @@ WITH row AS (
     --3.2 總計成績 總分 班排名
     --3.3 總計成績 總分 類別1排名
     --3.4 總計成績 總分 類別2排名
+    --3.5 總計成績 總分 科排名
     --4.1 總計成績 平均 年排名
     --4.2 總計成績 平均 班排名
+    --4.5 總計成績 平均 科排名
     --4.3 總計成績 平均 類別1排名
     --4.4 總計成績 平均 類別2排名
     --5.1 總計成績 加權總分 年排名
     --5.2 總計成績 加權總分 班排名
+    --5.5 總計成績 加權總分 科排名
     --5.3 總計成績 加權總分 類別1排名
     --5.4 總計成績 加權總分 類別2排名
     --6.1 總計成績 加權平均 年排名
     --6.2 總計成績 加權平均 班排名
+    --6.5 總計成績 加權平均 科排名
     --6.3 總計成績 加權平均 類別1排名
     --6.4 總計成績 加權平均 類別2排名
 
